3809e74a0063c143a80340e95e75b7818cc35a1b
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2019-12-31 08:29:58 +0900

    first

---
+++ Other.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtCoder
+{
+    class Other
+    {
+        //最大公約数
+        public static int Gcd(int a, int b)
+        {
+            if (a < b)
+            {
+                return Gcd(b, a);
+            }
+            while (b != 0)
+            {
+                var reminder = a % b;
+                a = b;
+                b = reminder;
+            }
+            return a;
+        }
+
+        //最小公倍数
+        public static int Lcm(int a, int b)
+        {
+            return a * b / Gcd(a, b);
+        }
+
+        //文字数を数える
+        public static int CountChar(string s, char c)
+        {
+            return s.Length - s.Replace(c.ToString(), "").Length;
+        }
+    }
+}
