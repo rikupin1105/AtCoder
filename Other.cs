c6b4cbdb38866795e5bf5b87cb28f6fdf87208fd
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2020-01-11 22:04:57 +0900

    順列生成を追加

--- Other.cs
+++ Other.cs
@@ -6,6 +6,47 @@ namespace AtCoder
 {
     class Other
     {
+        //順列生成 https://webbibouroku.com/Blog/Article/cs-permutation
+        static List<T[]> AllPermutation<T>(params T[] array) where T : IComparable
+        {
+            var a = new List<T>(array).ToArray();
+            var res = new List<T[]>();
+            res.Add(new List<T>(a).ToArray());
+            var n = a.Length;
+            var next = true;
+            while (next)
+            {
+                next = false;
+
+                // 1
+                int i;
+                for (i = n - 2; i >= 0; i--)
+                {
+                    if (a[i].CompareTo(a[i + 1]) < 0) break;
+                }
+                // 2
+                if (i < 0) break;
+
+                // 3
+                var j = n;
+                do
+                {
+                    j--;
+                } while (a[i].CompareTo(a[j]) > 0);
+
+                if (a[i].CompareTo(a[j]) < 0)
+                {
+                    // 4
+                    var tmp = a[i];
+                    a[i] = a[j];
+                    a[j] = tmp;
+                    Array.Reverse(a, i + 1, n - i - 1);
+                    res.Add(new List<T>(a).ToArray());
+                    next = true;
+                }
+            }
+            return res;
+        }
         //最大公約数
         public static int Gcd(int a, int b)
         {
